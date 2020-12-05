e0f2979ae223f44f369081ba172c01aef68afcdc
HenrikFalke <henrik.falke@rwth-aachen.de>
2020-12-05 12:43:27 +0100

    changes the model

--- queuemining/forms.py
+++ queuemining/forms.py
@@ -12,12 +12,3 @@ class SelectionForm(forms.ModelForm):
     class Meta:
         model = Table
         fields = ('timeframe', 'unit',)
-    UNIT_CHOICES = (
-        ("N", "---"),
-        ("H", "Hour"),
-        ("D", "Day"),
-        ("W", "Week"),
-        ("M", "Month")
-    )
-    timeframe = forms.IntegerField(label='timeframe', initial = 1)
-    unit = forms.ChoiceField(label='unit', choices = UNIT_CHOICES)
