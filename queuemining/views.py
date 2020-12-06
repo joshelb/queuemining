1e6c4b776e714416d4f548badf4a071d50462ef2
Baris Aktas <baris.aktas@rwth-aachen.de>
2020-12-06 14:45:06 +0100

    Adding the view and html template for viewing tables, also an example csv file for table format

--- queuemining/views.py
+++ queuemining/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 import random
+import pandas as pd
 
 from django.http import HttpResponse
 from . import forms
-from.models import Document
+from .models import Document
 from django.template import loader
+import csv
+import os
+from django.conf import settings
+
+
+def view_table(request):
+    csv_fp = open(os.path.join(settings.BASE_DIR, 'test_csv.csv'))
+    reader = csv.DictReader(csv_fp, delimiter=",")
+    fieldnames = [i for i in reader.fieldnames]
+    data = [i for i in reader]
+    context = {'data': data, 'fields': fieldnames}
+    return render(request, 'table_2.html', context)
 
 
 def importLogs(request):
     random.seed(10)
     if request.method == 'POST':
-        form = forms.DocumentForm(request.POST,request.FILES)
+        form = forms.DocumentForm(request.POST, request.FILES)
 
         if form.is_valid():
             name = form.cleaned_data['document']
@@ -19,17 +32,17 @@ def importLogs(request):
             save = form.save()
             id = save.pk
             print(id)
-            context = {'form' : form}
+            context = {'form': form}
         else:
             form = forms.DocumentForm()
             text = "This fileformat is not supported. Please use a CSV or XES file."
-            context = {'form' : form, 'text' : text}
+            context = {'form': form, 'text': text}
 
 
 
     else:
         form = forms.DocumentForm()
-        context = {'form' : form}
+        context = {'form': form}
 
     template = loader.get_template('main.html')
     return HttpResponse(template.render(context, request))
