080e6e6366d0578eb9ab6985077efe4d3d0a967a
joshelb <joshi31@gmx.de>
2021-01-09 17:02:33 +0100

    wezhw

---
+++ queuemining/processmining/testing.py
@@ -0,0 +1,85 @@
+import pm4py
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.algo.filtering.log.attributes import attributes_filter
+from pm4py.algo.filtering.log.timestamp import timestamp_filter
+import numpy as np
+import pandas as pd
+from datetime import timedelta, date, datetime
+import math
+
+log = xes_importer.apply('running-example.xes')
+
+event_stream = pm4py.convert_to_event_stream(log)
+
+get_activities = attributes_filter.get_attribute_values(log, "concept:name")
+
+
+
+
+for i in event_stream:
+    print(i)
+    print(type(i))
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+def creatTables(log):
+
+
+
+
+    get_activities = attributes_filter.get_attribute_values(log, "concept:name")
+
+    df = pd.DataFrame(columns=['Numebr of cases in the queue','Average Service Time','Average waiting time','Number of resources','Capacity of the activity'],
+                      )
+    for activity in get_activities:
+        print(activity)
+        tracefilter_log_pos = attributes_filter.apply(log, [activity],
+                                                      parameters={
+                                                          attributes_filter.Parameters.ATTRIBUTE_KEY: "concept:name",
+                                                          attributes_filter.Parameters.POSITIVE: True})
+        new_row  = pd.Series(data={'Numebr of cases in the queue':len(tracefilter_log_pos)}, name=activity)
+        df = df.append(new_row,ignore_index=False)
+
+        print(df)
+        print("        ###################              ")
+        for i in tracefilter_log_pos:
+
+            print(i, "\n")
+
+        print("        ###################              ")
+
+    return
+
+
+def filtertimerange(log):
+    old = log[0][0]["time:timestamp"]
+    new = None
+    old2 = old
+    new2 = None
+    for trace  in log:
+        for event in trace:
+            new = event["time:timestamp"]
+            new2 = new
+            if new < old:
+                old = new
+            elif new2 > old2:
+
+                old2 = new2
+
+    return old, old2
+
+def timerange(start_date, end_date,timestep):
+    for n in range(int(math.ceil(int(int((end_date - start_date).seconds)/60)/timestep))):
+        print(range(int(math.ceil(int(int((end_date - start_date).seconds)/60)/timestep))))
+        yield start_date + timedelta(n)
+
+def timesplitting(log,rangestart,rangeend,timestep):
+    for single_step in timerange(rangestart, rangeend,timestep):
+        print(single_step)
+
+
+
+
+start_date , end_date = filtertimerange(log)
+timesplitting(log,start_date,end_date,100)
+
+
+creatTables(log)
