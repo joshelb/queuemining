5eea61e7d496279fd2fc31f5b09f96110674805d
HenrikFalke <henrik.falke@rwth-aachen.de>
2021-01-12 17:25:25 +0100

    Checking if Submitted Business hours are valid

--- queuemining/utils.py
+++ queuemining/utils.py
@@ -8,7 +8,12 @@ def data_valid(form):
     """A shortcut function, that returns true, if the SelectionForm and DocForm are valid and the selection
     given by the user is usable (ergo the timeframe is greater than 0 and a unit was selected) """
     output = False
-    if form.is_valid() and not form.cleaned_data['unit'] == "N" and form.cleaned_data['timeframe'] > 0:
+    if form.is_valid() and not form.cleaned_data['unit'] == "N" \
+            and form.cleaned_data['timeframe'] > 0\
+            and form.cleaned_data['day_start'] > form.cleaned_data['day_end']\
+            and 0 <= form.cleaned_data['day_start'] <= 24\
+            and 0 <= form.cleaned_data['day_end'] <= 24\
+            and len(form.cleaned_data['weekends']) < 7:
         output = True
     return output
 
