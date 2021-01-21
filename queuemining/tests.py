f57b4590858366013f7cb5678286db7e12c69e96
joshelb <joshi31@gmx.de>
2021-01-21 13:38:37 +0100

    fwqfwafawgawWGAgW

--- queuemining/tests.py
+++ queuemining/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
-from .models import Data
+from .models import Data,TimeStep
+from datetime import timezone
 import os
 import sys
 from shutil import copyfile
@@ -10,26 +11,9 @@ import ntpath
 
 class InputTestCase(TestCase):
     def setUp(self):
-        self.c = Client()
-        sys.stderr.write(repr(os.getcwd()) + '\n')
-        os.chdir('media/documents')
-        path = os.getcwd()
-        self.file_root = next(os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)))
-        copyfile(self.path_leaf(self.file_root), '../documentstest/test.xes')
-        os.chdir('../documentstest/')
-        self.new_file = "test.xes"
-        sys.stderr.write(repr(self.new_file) + '\n')
-
-
-    def path_leaf(self,path):
-        head, tail = ntpath.split(path)
-        return tail or ntpath.basename(head)
-
-
-    def test_runInputTest(self):
-        with open(self.new_file) as fd:
-            self.c.post('/queuemining/', {'document': fd, 'offdays': ["0"], 'day_start': 2, 'day_end': 20,
-                                          'timeframe': 2, 'unit': "D"})
-            data = Data.objects.filter(document='documents/test.xes')
-        os.remove(self.new_file)
-        self.assertEqual(len(data), 1)
+        data = Data.objects.create(id = 1, document = None)
+        object = TimeStep.objects.create(unit = "H")
+        data.timestep.add(object)
+    def testdataobject(self):
+        a = Data.objects.get(id = 1).timestep.all()
+        self.assertEqual(a[0].unit, "H")
