ef7a9fd1f26e96b0e9253dc976c67c4f57165a4d
HenrikFalke <henrik.falke@rwth-aachen.de>
2021-01-21 13:50:21 +0100

    Update tests.py

--- queuemining/tests.py
+++ queuemining/tests.py
@@ -11,9 +11,10 @@ import ntpath
 
 class InputTestCase(TestCase):
     def setUp(self):
-        data = Data.objects.create(id = 1, document = None)
-        object = TimeStep.objects.create(unit = "H")
+        data = Data.objects.create(id=1, document=None)
+        object = TimeStep.objects.create(unit="H")
         data.timestep.add(object)
-    def testdataobject(self):
-        a = Data.objects.get(id = 1).timestep.all()
+
+    def testDataObject(self):
+        a = Data.objects.get(id=1).timestep.all()
         self.assertEqual(a[0].unit, "H")
